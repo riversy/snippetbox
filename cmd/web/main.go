e5b488a6b1f9de20b70090c90f2b55ce8ec02147
Igor Goltsov <riversy@gmail.com>
2020-06-19 01:23:00 +0300

    Add -addr parameter to configure app on starting point

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,32 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
+type Config struct {
+	Addr      string
+	StaticDir string
+}
+
 func main() {
+	cfg := new(Config)
+	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.StaticDir, "static-dir", "./ui/static/", "Path to static assets")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.StaticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileServer)))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s\n", cfg.Addr)
+	err := http.ListenAndServe(cfg.Addr, mux)
 	log.Fatal(err)
 }
