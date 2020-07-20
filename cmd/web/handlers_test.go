d655f3b7c40000799bcfed1677190cb5710560e8
Igor Goltsov <riversy@gmail.com>
2020-07-21 00:45:28 +0300

    Use integration tests as a Ping Test

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestPing(t *testing.T) {
-	rr := httptest.NewRecorder()
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	app := &application{
+		errorLog: log.New(ioutil.Discard, "", 0),
+		infoLog:  log.New(ioutil.Discard, "", 0),
+	}
+
+	ts := httptest.NewTLSServer(app.routes())
+	defer ts.Close()
+
+	rs, err := ts.Client().Get(ts.URL + "/ping")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	ping(rr, r)
-	rs := rr.Result()
 	if rs.StatusCode != http.StatusOK {
 		t.Errorf("want %d; got %d", http.StatusOK, rs.StatusCode)
 	}
@@ -24,6 +30,7 @@ func TestPing(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	if string(body) != "OK" {
 		t.Errorf("want body to equal %q", "OK")
 	}
